ce542add4953dade9373822d383b875ec53ec307
Redeemer40 <victorjonah199@gmail.com>
2020-04-11 13:10:21 +0100

    lunch

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
-const logger = require('./../config/logger');
-const { mongo, env } = require('./vars');
+const mongoose = require("mongoose");
+const logger = require("./../config/logger");
+const { mongo, env } = require("./vars");
 
 // set mongoose Promise to Bluebird
 mongoose.Promise = Promise;
 
 // Exit application on error
-mongoose.connection.on('error', (err) => {
+mongoose.connection.on("error", (err) => {
   logger.error(`MongoDB connection error: ${err}`);
   process.exit(-1);
 });
 
 // print mongoose logs in dev env
-if (env === 'development') {
-  mongoose.set('debug', true);
+if (env === "development") {
+  mongoose.set("debug", true);
 }
 
 /**
@@ -31,6 +31,6 @@ exports.connect = () => {
       useUnifiedTopology: true,
       useFindAndModify: false,
     })
-    .then(() => console.log('mongoDB connected...'));
+    .then(() => console.log("mongoDB connected..."));
   return mongoose.connection;
 };
