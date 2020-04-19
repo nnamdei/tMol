5c6bac2a50bb7beead52576c743806077cf89d7e
Redeemer40 <victorjonah199@gmail.com>
2020-04-19 20:54:18 +0100

    ...

---
+++ src/api/services/giftcards/giftcards.js
@@ -0,0 +1,115 @@
+const cardDetails = [
+  {
+    categoryName: "AMAZON",
+    image: "./images/Amazon.png",
+    cardCategory: [
+      {
+        title: "Cash Reciept",
+      },
+      {
+        title: "Debit Reciept",
+      },
+      {
+        title: "Ecode",
+      },
+    ],
+  },
+  {
+    categoryName: "STEAM",
+    image: "./images/steam.png",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "EBay",
+    image: "./images/ebay.png",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "Home Depot",
+    image: "./images/homedepot.jpeg",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "ITunes",
+    image: "./images/itunes.jpg",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "Walmart",
+    image: "./images/walmart.jpeg",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "Vanilla",
+    image: "./images/walmart.jpeg",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "APPLE STORE",
+    image: "./images/apple.png",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+  {
+    categoryName: "American Express (AMEX)",
+    image: "./images/amex.png",
+    cardCategory: [
+      {
+        title: "Physical cards",
+      },
+      {
+        title: "Ecodes",
+      },
+    ],
+  },
+];
+
+module.exports = cardDetails;
