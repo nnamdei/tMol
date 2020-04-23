6f4c9213e10e92e9c2bcefe834399449c1c38424
Redeemer40 <victorjonah199@gmail.com>
2020-04-24 00:20:34 +0100

    accept/decline transactions

---
+++ src/api/controllers/admin.controller.js
@@ -0,0 +1,165 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable arrow-parens */
+const httpStatus = require("http-status");
+const Transaction = require("../models/transaction.model");
+const User = require("../models/user.model");
+const emailProvider = require("../services/emails/emailProvider");
+/**
+ * Load user and append to req.
+ * @public
+ */
+exports.load = async (req, res, next, id) => {
+  try {
+    const user = await User.get(id);
+    req.locals = { user };
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Get user
+ * @public
+ */
+exports.get = (req, res) => res.json(req.locals.user.transform());
+
+/**
+ * Get logged in user info
+ * @public
+ */
+exports.loggedIn = (req, res) => res.json(req.user.transform());
+
+/**
+ * Get user list
+ * @public
+ */
+exports.listUsers = async (req, res, next) => {
+  try {
+    console.log(req.query);
+
+    const users = await User.list(req.query);
+    const transformedUsers = users.map((user) => user.transform());
+    res.json(transformedUsers);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Create new user
+ * @public
+ */
+exports.createUser = async (req, res, next) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+    res.status(httpStatus.CREATED);
+    emailProvider(user);
+    res.json(savedUser.transform());
+  } catch (error) {
+    next(User.checkDuplicateEmail(error));
+  }
+};
+
+/**
+ * Update existing user
+ * @public
+ */
+exports.updateUser = async (req, res, next) => {
+  try {
+    const { id } = req.user;
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      useFindAndModify: false,
+      new: true,
+    });
+
+    if (updatedUser) {
+      return res.status(httpStatus.CREATED).json({
+        message: "User updated",
+        updatedUser,
+      });
+    }
+    throw new Error("Update unsuccesful");
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Delete user
+ * @public
+ */
+exports.removeUser = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    await User.findOneAndRemove(id);
+    return res.status(httpStatus.OK).json({
+      message: "User deleted",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+/**
+ * Get transaction list
+ * @public
+ */
+exports.listTransanction = async (req, res, next) => {
+  try {
+    console.log(req.query);
+
+    const transactions = await Transaction.list(req.query);
+    res.json(transactions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Approve a transaction
+ * @public
+ */
+exports.approveTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.query;
+    const specificTransaction = await Transaction.findById(id);
+    if (
+      specificTransaction.status === "Pending" ||
+      specificTransaction.status === "Declined"
+    ) {
+      specificTransaction.status = "Approved";
+      await specificTransaction.save();
+      res.json({
+        message: "Transaction Approved",
+        specificTransaction,
+      });
+    }
+    throw new Error("Error");
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ * Reject a transaction
+ * @public
+ */
+exports.rejectTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.query;
+    const specificTransaction = await Transaction.findById(id);
+    if (specificTransaction.status === "Pending") {
+      specificTransaction.status = "Declined";
+      await specificTransaction.save();
+      res.json({
+        message: "Transaction Declined",
+        specificTransaction,
+      });
+    }
+    throw new Error("Transaction was already approved");
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ src/api/routes/v1/admin.route.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const validate = require("express-validation");
+const controller = require("../../controllers/admin.controller");
+const { authorize, ADMIN, LOGGED_USER } = require("../../middlewares/auth");
+const { listUsers, createUser } = require("../../validations/user.validation");
+
+const router = express.Router();
+
+/**
+ * Load user when API with userId route parameter is hit
+ */
+router.param("userId", controller.load);
+
+router
+  .route("/user")
+  .get(authorize(ADMIN), validate(listUsers), controller.listUsers)
+  .post(authorize(ADMIN), validate(createUser), controller.createUser);
+
+router.route("/user/:id").delete(authorize(ADMIN), controller.removeUser);
+
+router.route("/profile").patch(authorize(LOGGED_USER), controller.updateUser);
+
+router.route("/transaction").get(authorize(ADMIN), controller.listTransanction);
+router
+  .route("/transaction/accept")
+  .patch(authorize(ADMIN), controller.approveTransaction);
+router
+  .route("/transaction/reject")
+  .patch(authorize(ADMIN), controller.rejectTransaction);
+module.exports = router;
