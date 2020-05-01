47591820e1918537d1abaed5f4a4276a38d68b79
emex4gman <emex4gman@gmail.com>
2020-05-01 15:22:39 +0100

    notification

--- src/api/controllers/notification.controller.js
+++ src/api/controllers/notification.controller.js
@@ -1,13 +1,18 @@
 const httpStatus = require("http-status");
-
+const { getFcmTokens, sendToDevice } = require('../services/firebaseMessage')
 exports.sendNotification = async (req, res, next) => {
   try {
     const { title, content } = req.body;
     const data = { title, content };
     if (data) {
+      let usersFcmTokens = await getFcmTokens('user')
+      if (usersFcmTokens.length != 0) {
+        await sendToDevice(usersFcmTokens, data, 'user')
+      }
       return res.status(httpStatus.CREATED).json({
         message: "Notification sent",
         data,
+        usersFcmTokens,
       });
     }
     throw new Error("Unsuccessful");
