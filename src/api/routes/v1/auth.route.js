0a9a89999ae72ef29359ef388ea2cbde27f09719
Redeemer40 <victorjonah199@gmail.com>
2020-05-02 10:44:10 +0100

    ...

--- src/api/routes/v1/auth.route.js
+++ src/api/routes/v1/auth.route.js
@@ -2,16 +2,16 @@ const express = require("express");
 const validate = require("express-validation");
 const controller = require("../../controllers/auth.controller");
 const services = require("../../services/updateBankDetails");
+const servicePassword = require("../../services/updatePassword");
 const { authorize, LOGGED_USER } = require("../../middlewares/auth");
 
 const oAuthLogin = require("../../middlewares/auth").oAuth;
 const {
   login,
   register,
-  oAuth,
   refresh,
   sendPasswordReset,
-  passwordReset,
+  updatePassword,
 } = require("../../validations/auth.validation");
 
 const router = express.Router();
@@ -100,55 +100,15 @@ router
   .post(validate(sendPasswordReset), controller.sendPasswordReset);
 
 router
-  .route("/reset-password")
-  .post(validate(passwordReset), controller.resetPassword);
+  .route("/change-password")
+  .patch(
+    validate(updatePassword),
+    authorize(LOGGED_USER),
+    servicePassword.updatePassword
+  );
 
 router
   .route("/bank-details")
   .patch(authorize(LOGGED_USER), services.updateBankDetails);
-
-/**
- * @api {post} v1/auth/facebook Facebook Login
- * @apiDescription Login with facebook. Creates a new user if it does not exist
- * @apiVersion 1.0.0
- * @apiName FacebookLogin
- * @apiGroup Auth
- * @apiPermission public
- *
- * @apiParam  {String}  access_token  Facebook's access_token
- *
- * @apiSuccess {String}  tokenType     Access Token's type
- * @apiSuccess {String}  accessToken   Authorization Token
- * @apiSuccess {String}  refreshToken  Token to get a new accessToken after expiration time
- * @apiSuccess {Number}  expiresIn     Access Token's expiration time in miliseconds
- *
- * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
- * @apiError (Unauthorized 401)  Unauthorized    Incorrect access_token
- */
-router
-  .route("/facebook")
-  .post(validate(oAuth), oAuthLogin("facebook"), controller.oAuth);
-
-/**
- * @api {post} v1/auth/google Google Login
- * @apiDescription Login with google. Creates a new user if it does not exist
- * @apiVersion 1.0.0
- * @apiName GoogleLogin
- * @apiGroup Auth
- * @apiPermission public
- *
- * @apiParam  {String}  access_token  Google's access_token
- *
- * @apiSuccess {String}  tokenType     Access Token's type
- * @apiSuccess {String}  accessToken   Authorization Token
- * @apiSuccess {String}  refreshToken  Token to get a new accpessToken after expiration time
- * @apiSuccess {Number}  expiresIn     Access Token's expiration time in miliseconds
- *
- * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
- * @apiError (Unauthorized 401)  Unauthorized    Incorrect access_token
- */
-router
-  .route("/google")
-  .post(validate(oAuth), oAuthLogin("google"), controller.oAuth);
 
 module.exports = router;
